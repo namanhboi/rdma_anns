7c8417a73f8272afc72ef04d12445497ba1b5508
Nam Anh <dangnamanh05@gmail.com>
2025-07-08 04:05:59 -0400

    Linked the gp-ann library that I created and finished graph partitioning loading

--- benchmark/setup.cpp
+++ benchmark/setup.cpp
@@ -12,12 +12,17 @@
 #include <parlay/parallel.h>
 #include <iostream>
 #include <cascade/service_client_api.hpp>
-
+#include "metis_io.h"
+#include "partition_prebuilt_graph.hpp"
+#include <cstddef>
 namespace po = boost::program_options;
 using namespace derecho::cascade;
 using namespace parlayANN;
 
 #define PROC_NAME "setup"
+#define WHOLE_GRAPH_SUBGROUP_INDEX 0
+
+// TODO still need to load the graph correctly: load the embedidng next to the neighbors id: /anns/shard_i/vector_j
 
 template<typename PointRange>
 void load_graph_into_shard(ServiceClientAPI& capi, const Graph<unsigned int> &G, const PointRange& Points, std::string shard_folder, int subgroup_index) {
@@ -59,15 +64,70 @@ void load_graph_into_shard(ServiceClientAPI& capi, const Graph<unsigned int> &G,
 
 
 
+struct vector_data {
+  uint8_t *embedding;
+  int dimension;
+  edgeRange<unsigned int> neighbors;
+};
+/**
+   Load each cluster (full length vector embedding and neighbor ids) onto a
+   shard on Cascade. subgroup id is WHOLE_GRAPH_SUBGROUP_INDEX
+   Key will be vector id, value will be as follows:
+   - sizeof(data_type) * dimension bytes is the embedding
+   - the next sizeof(unsigned int) * G[i].size() bytes are the ids of the
+   neighbors.
+   - will need to change from volatile to persistent datastore, also need to figure out how to pin certain kv pairs in cache (the ones close to starting point);
+*/
+template <typename PointRange>
+void load_whole_graph(ServiceClientAPI &capi, const Graph<unsigned int> &G,
+                      const PointRange &Points, const Clusters &clusters) {
+
+  using Point = typename PointRange::Point;
+  using data_type = typename Point::distanceType;
+
+
+  int dimension = Points.dimension();
+
+  if (clusters.size() > 255) {
+    throw std::invalid_argument("Too many clusters mate, doesn't fit into 1 byte");
+  }
+  for (int i = 0; i < clusters.size(); i++) {
+    std::string cluster_folder = "/anns/cluster_" + std::to_string(i);
+    capi.template create_object_pool<VolatileCascadeStoreWithStringKey>(cluster_folder, WHOLE_GRAPH_SUBGROUP_INDEX, HASH, {}, "cluster[0-9]+");
+    parlay::parallel_for(0, clusters[i].size(), [&](size_t j) {
+      uint32_t vector_id = clusters[i][j];
+      size_t num_byte_emb = sizeof(data_type) * dimension;
+      size_t num_byte_neighbors = sizeof(unsigned int) * G[vector_id].size();
+      size_t num_byte_cluster_mapping = G[vector_id].size(); // contigous chunk of memory mapping a neighbor to its cluster.
+      size_t num_byte_object = num_byte_emb + num_byte_neighbors ;
+      std::vector<std::byte> data(num_byte_object);
+      std::memcpy(data.data(), Points.location(i), num_byte_emb);
+      std::memcpy(data.data() + dimension * sizeof(data_type),
+                  G[vector_id].begin(), num_byte_neighbors);
+      ObjectWithStringKey vector_data;
+      vector_data.key = cluster_folder + "/vector_" + std::to_string(vector_id);
+      vector_data.previous_version = INVALID_VERSION;
+      vector_data.previous_version_by_key = INVALID_VERSION;
+      vector_data.blob = Blob(reinterpret_cast<const uint8_t*>(data.data()), num_byte_object);
+      auto result = capi.put(vector_data, false);
+      for (auto &reply_future : result.get()) {
+        auto reply = reply_future.second.get();
+      }
+    });
+    std::cout << "Done with cluster " << i << "/" << clusters.size() - 1<< std::endl;
+  }
+}
 
 
 int main(int argc, char** argv) {
   po::options_description desc("Program Input");
   desc.add_options()
     ("help,h", "show help message") 
-    ("graph-path,G", po::value<std::vector<std::string>>()->required(), "Path to graph segments to be loaded into Cascade. The segments will be loaded into object pools /anns/shard_{i}, where i is the index of the segment. For the ith graph segment, we will load the neighbors of the jth vector into /anns/shard_{i}/neighbors_{j} and the full length vector embedding in /anns/shard_{i}/emb_{j}. Embedding is from the base-path.")
+    ("graph-path,G", po::value<std::string>()->required(), "Path to whole graph to be loaded into Cascade. The graph will be partioned into num_clusters via the balanced graph partitioning algorithm. The non overlapping partitions will be loaded into object pools /anns/cluster_{i}, where i is the index of the partition. For the ith graph partition, we will load the data of the jth vector into /anns/cluster_{i}/vector_{j} and Embedding is from the base-path.")
     ("base-path,B", po::value<std::string>()->required(), "Path to the full vector data be loaded into Cascade.")
     ("data-type,T", po::value<std::string>()->required(), "Data type of vectors, could be float, uint8, int8")
+    ("num-clusters,N", po::value<int>()->required(), "Number of partitions to divide the whole graph into")
+    ("output-clusters,OC", po::value<std::string>()->required(), "Path to write the clusters")
     ("dist-func,F", po::value<std::string>()->required(), "Distance function, could be Euclidian or ?. RN only support Euclidian");
   po::variables_map vm;
 
@@ -82,37 +142,30 @@ int main(int argc, char** argv) {
   if (dist_func != "Euclidian") throw std::invalid_argument("only support euclidian");
   std::string data_type = vm["data-type"].as<std::string>();
   std::string base_path = vm["base-path"].as<std::string>();
-  const std::vector<std::string> &graph_path_list = vm["graph-path"].as<std::vector<std::string>>();
-  
-  ServiceClientAPI &capi = ServiceClientAPI::get_service_client();
+  std::string graph_path = vm["graph-path"].as<std::string>();
+  Graph<unsigned int> G(graph_path.data());
 
+  int num_clusters = vm["num-clusters"].as<int>();
+
+  ServiceClientAPI &capi = ServiceClientAPI::get_service_client();
+  std::cout << "Starting to build clusters" << std::endl;
+  const Clusters &clusters = get_clusters_from_graph(G, num_clusters);
+  std::cout << "Done building clusters" << std::endl;
+  std::cout << "total " << clusters.size() << " clusters" << std::endl;
+  WriteClusters(clusters, vm["output-clusters"].as<std::string>());
   if (data_type == "float") {
     using PR = PointRange<Euclidian_Point<float>>;
     PR Points(base_path.data());
-    for (int i = 0; i < graph_path_list.size(); i++) {
-#define GRAPH_FOLDER_PREFIX "/anns/shard_"
-      std::string shard_folder = GRAPH_FOLDER_PREFIX + std::to_string(i);
-      std::string graph_path = graph_path_list[i];
-      load_graph_into_shard<PR>(capi,Graph<unsigned int>(graph_path.data()), Points, shard_folder, i);
-    }
+    load_whole_graph(capi, G, Points, clusters);
+
   } else if (data_type == "uint8") {
     using PR = PointRange<Euclidian_Point<uint8_t>>;
     PR Points(base_path.data());
-    for (int i = 0; i < graph_path_list.size(); i++) {
-#define GRAPH_FOLDER_PREFIX "/anns/shard_"
-      std::string shard_folder = GRAPH_FOLDER_PREFIX + std::to_string(i);
-      std::string graph_path = graph_path_list[i];
-      load_graph_into_shard<PR>(capi,Graph<unsigned int>(graph_path.data()), Points, shard_folder, i);
-    }
+    load_whole_graph(capi, G, Points, clusters);
   } else if (data_type == "int8") {
     using PR = PointRange<Euclidian_Point<int8_t>>;
     PR Points(base_path.data());
-    for (int i = 0; i < graph_path_list.size(); i++) {
-#define GRAPH_FOLDER_PREFIX "/anns/shard_"
-      std::string shard_folder = GRAPH_FOLDER_PREFIX + std::to_string(i);
-      std::string graph_path = graph_path_list[i];
-      load_graph_into_shard<PR>(capi,Graph<unsigned int>(graph_path.data()), Points, shard_folder, i);
-    }
+    load_whole_graph(capi, G, Points, clusters);
   }
   std::cout <<"done" << std::endl;
 
