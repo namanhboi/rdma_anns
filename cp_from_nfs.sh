08eb47a9998223e7913b0f61904b5bc79a9273d5
Nam Anh <dangnamanh05@gmail.com>
2025-11-03 22:16:26 -0500

    Updated sh file with correct path for copying from nfs

--- cp_from_nfs.sh
+++ cp_from_nfs.sh
@@ -4,13 +4,19 @@ NFS_PREFIX=/nfs/anngraphs/bigann/100M/
 FOLDER_NAME=$1
 ID=$2
 
+# Determine suffix based on folder name
+if [[ "$FOLDER_NAME" == *"partitions"* ]]; then
+    SUFFIX="_partition${ID}"
+elif [[ "$FOLDER_NAME" == *"clusters"* ]]; then
+    SUFFIX="_cluster${ID}"
+else
+    echo "Error: Folder name must contain 'partitions' or 'clusters'"
+    exit 1
+fi
 
 MYDATA_FILES_PREFIX="${MYDATA_PREFIX}/${FOLDER_NAME}/pipeann_100M"
 NFS_FILES_PREFIX="${NFS_PREFIX}/${FOLDER_NAME}/pipeann_100M"
-cp "${NFS_FILES_PREFIX}${ID}_disk.index" "${MYDATA_FILES_PREFIX}_disk.index"
-cp "${NFS_FILES_PREFIX}${ID}_ids_uint32.bin" "${MYDATA_FILES_PREFIX}_ids_uint32.bin"
-cp "${NFS_FILES_PREFIX}_partition_assignment.bin" "${MYDATA_FILES_PREFIX}_partition_assignment.bin
 
-
-
-
+cp "${NFS_FILES_PREFIX}${SUFFIX}_disk.index" "${MYDATA_FILES_PREFIX}${SUFFIX}_disk.index"
+cp "${NFS_FILES_PREFIX}${SUFFIX}_ids_uint32.bin" "${MYDATA_FILES_PREFIX}${SUFFIX}_ids_uint32.bin"
+cp "${NFS_FILES_PREFIX}_partition_assignment.bin" "${MYDATA_FILES_PREFIX}_partition_assignment.bin"
