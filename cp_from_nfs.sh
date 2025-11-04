8bb249bcc9e99e0af45d87864b65df58c6bcd1f1
Nam Anh <dangnamanh05@gmail.com>
2025-11-03 22:19:42 -0500

    Changed sh for copying again

--- cp_from_nfs.sh
+++ cp_from_nfs.sh
@@ -4,19 +4,27 @@ NFS_PREFIX=/nfs/anngraphs/bigann/100M/
 FOLDER_NAME=$1
 ID=$2
 
-# Determine suffix based on folder name
+MYDATA_FILES_PREFIX="${MYDATA_PREFIX}/${FOLDER_NAME}/pipeann_100M"
+NFS_FILES_PREFIX="${NFS_PREFIX}/${FOLDER_NAME}/pipeann_100M"
+
+# Determine suffix and copy logic based on folder name
 if [[ "$FOLDER_NAME" == *"partitions"* ]]; then
     SUFFIX="_partition${ID}"
+    cp "${NFS_FILES_PREFIX}${SUFFIX}_disk.index" "${MYDATA_FILES_PREFIX}${SUFFIX}_disk.index"
+    cp "${NFS_FILES_PREFIX}${SUFFIX}_ids_uint32.bin" "${MYDATA_FILES_PREFIX}${SUFFIX}_ids_uint32.bin"
+    cp "${NFS_FILES_PREFIX}_partition_assignment.bin" "${MYDATA_FILES_PREFIX}_partition_assignment.bin"
+    
 elif [[ "$FOLDER_NAME" == *"clusters"* ]]; then
     SUFFIX="_cluster${ID}"
+    # Copy all files with the cluster suffix except those ending with _graph
+    for file in "${NFS_FILES_PREFIX}${SUFFIX}"*; do
+        if [[ ! "$file" == *"_graph" ]]; then
+            filename=$(basename "$file")
+            cp "$file" "${MYDATA_FILES_PREFIX}${SUFFIX}${filename#*${SUFFIX}}"
+        fi
+    done
+    
 else
     echo "Error: Folder name must contain 'partitions' or 'clusters'"
     exit 1
 fi
-
-MYDATA_FILES_PREFIX="${MYDATA_PREFIX}/${FOLDER_NAME}/pipeann_100M"
-NFS_FILES_PREFIX="${NFS_PREFIX}/${FOLDER_NAME}/pipeann_100M"
-
-cp "${NFS_FILES_PREFIX}${SUFFIX}_disk.index" "${MYDATA_FILES_PREFIX}${SUFFIX}_disk.index"
-cp "${NFS_FILES_PREFIX}${SUFFIX}_ids_uint32.bin" "${MYDATA_FILES_PREFIX}${SUFFIX}_ids_uint32.bin"
-cp "${NFS_FILES_PREFIX}_partition_assignment.bin" "${MYDATA_FILES_PREFIX}_partition_assignment.bin"
