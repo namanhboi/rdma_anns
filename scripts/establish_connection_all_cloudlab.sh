15a536fc1bff4904ec70708d68e3efd4f7c4b609
Nam Anh <dangnamanh05@gmail.com>
2026-03-12 00:55:18 -0400

    Fixed typo

--- scripts/establish_connection_all_cloudlab.sh
+++ scripts/establish_connection_all_cloudlab.sh
@@ -1,7 +1,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ${SCRIPT_DIR}/cloudlab_addresses.sh
 
-for CLOUDLAB_HOST in ${ALL_CLOULAB_HOSTS[@]}; do
+for CLOUDLAB_HOST in ${ALL_CLOUDLAB_HOSTS[@]}; do
     echo "Establishing connection to $CLOUDLAB_HOST"
     ssh $CLOUDLAB_HOST
 done
