1a7ee5a66f480d7d93aca4a37aa74e1ff20b9338
Nam Anh <dangnamanh05@gmail.com>
2026-03-25 09:10:49 -0400

    Fixed error in random partition generation

--- src/state_send/create_random_partition_files.cpp
+++ src/state_send/create_random_partition_files.cpp
@@ -2,36 +2,50 @@
 #include <iostream>
 #include <random>
 #include <algorithm>
+#include <vector>
 #include "disk_utils.h"
 
 int main(int argc, char **argv) {
-  std::string index_path_prefix;
-  uint32_t num_points, num_partitions;
-  
-
   if (argc != 4) {
-    std::cout << "Usage: <index_path_prefix (doesn't include 'partititon' in path name> <num_points> <num_partitions>" << std::endl;
+    std::cout << "Usage: " << argv[0] << " <index_path_prefix> <num_points> <num_partitions>" << std::endl;
     return 1;
   }
+
+  // 1. Initialize variables from command line arguments
+  std::string index_path_prefix = argv[1];
+  uint32_t num_points = std::stoul(argv[2]);
+  uint32_t num_partitions = std::stoul(argv[3]);
+
+  if (num_partitions == 0) {
+    std::cerr << "Error: num_partitions must be greater than 0." << std::endl;
+    return 1;
+  }
+
   std::random_device rd;
   std::mt19937 g(rd());
   std::vector<uint32_t> ids;
   ids.reserve(num_points);
-  std::vector<std::vector<uint32_t>> partitions(num_partitions);
   
+  std::vector<std::vector<uint32_t>> partitions(num_partitions);
   uint32_t partition_size = num_points / num_partitions;
     
   for (uint32_t i = 0; i < num_points; i++) {
     ids.push_back(i);
   }
+  
   std::shuffle(ids.begin(), ids.end(), g);
   
   for (uint32_t i = 0; i < num_points; i++) {
-    uint32_t current_partition = i / partition_size;
-    partitions[current_partition].push_back(i);
+    // 2. Prevent Out-of-Bounds by capping the partition index at the last partition.
+    // Any "remainder" points will be added to the final partition.
+    uint32_t current_partition = std::min(i / partition_size, num_partitions - 1);
+    
+    // 3. Push the SHUFFLED id, not the loop index 'i'
+    partitions[current_partition].push_back(ids[i]);
   }
+  
   write_partitions_to_loc_files(partitions, index_path_prefix);
   create_partition_assignment_file(index_path_prefix, num_partitions);
+  
   return 0;
 }
-
